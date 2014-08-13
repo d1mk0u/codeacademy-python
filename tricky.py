38e3ac85592d06bbedcca72bf4a8aaf367688ddf
DmitriS <dimheld@gmail.com>
2014-08-13 23:17:34 +0300

    Update tricky.py

--- tricky.py
+++ tricky.py
@@ -1,8 +1,8 @@
-let = []
 def digit_sum(n):
+    let = []
     n = str(n)
     for i in n:
         i = int(i)
         let.append(i)
     return sum(let)
-print digit_sum(5555)
+print digit_sum(raw_input("Enter a positive integer:"))
